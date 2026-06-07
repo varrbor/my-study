5fab9d405c0b3bc7efa16062f3ccc00d1cb6809d
varrbor <varrbor@ukr.net>
2026-06-07 16:30:27 +0200

    added system design

---
+++ SYSTEM-DESIGN/NETWORKING-adn-request-flow/client-server-request-flow.js
@@ -0,0 +1,4 @@
+// client-server-request-flow
+https://prnt.sc/9Q3_x8-zl2oN
+
+'/Users/varrbor/Pictures/Screenshot at Jun 07 16-12-39.png'
---
+++ SYSTEM-DESIGN/NETWORKING-adn-request-flow/client-server-request-flow.md
@@ -0,0 +1,1 @@
+<img src="../../assets/clien-server-request-flow.png"  />
